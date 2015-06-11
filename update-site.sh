bb59d33c04c992e13b7043586311da9420ac1a8b
Yuhuang Hu <duguyue100@gmail.com>
2015-06-11 14:04:28 +0800

    Thu Jun 11 14:04:27 MYT 2015: update site: site update

---
+++ update-site.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+now="$(date): update site: jekyll setup"
+
+git add -A
+
+git commit -m "$now"
+git push origin master
+
+jekyll build --destination ../duguyue100.github.io
+
+cd ../duguyue100.github.io/
+
+site_now="$(date): update site: site update"
+
+git add -A
+
+git commit -m "$site_now"
+
+git push origin master
